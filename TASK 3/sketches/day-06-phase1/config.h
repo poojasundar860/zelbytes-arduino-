65035a6f9346769c0720c3949050865a7827781d
poojasundar860 <poojasundar860@gmail.com>
2026-06-04 10:46:44 +0530

    Update config.h

--- TASK 3/sketches/day-06-phase1/config.h
+++ TASK 3/sketches/day-06-phase1/config.h
@@ -1,1 +1,19 @@
+#ifndef CONFIG_H
+#define CONFIG_H
 
+// Pin assignments
+#define BTN_MANUAL  2
+#define LED_STATUS  13
+#define RELAY_VALVE 7
+
+// Serial
+#define BAUD_RATE 9600
+
+// Debounce
+#define DEBOUNCE_MS 50
+
+// Relay logic (active-low module)
+#define RELAY_ON  LOW
+#define RELAY_OFF HIGH
+
+#endif
