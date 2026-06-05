ee60ca32d39ea1b1cedc03e0a7463f3c56ea8ab8
poojasundar860 <poojasundar860@gmail.com>
2026-06-05 16:01:42 +0530

    Update config.h

--- TASK 4/sketches/day-11-sensor_logging/config.h
+++ TASK 4/sketches/day-11-sensor_logging/config.h
@@ -1,1 +1,22 @@
+#ifndef CONFIG_H
+#define CONFIG_H
 
+// LDR
+#define LDR_PIN A0
+
+// Soil Moisture
+#define SOIL_PIN A1
+#define SOIL_PWR 7
+
+// DHT22
+#define DHT_PIN 4
+#define DHT_TYPE DHT22
+
+// HC-SR04
+#define TRIG_PIN 9
+#define ECHO_PIN 10
+
+// Serial
+#define BAUD_RATE 9600
+
+#endif
