ce8650a7a0395925737519f28477d7c699891551
poojasundar860 <poojasundar860@gmail.com>
2026-06-05 16:03:17 +0530

    Update calibration.h

--- TASK 4/sketches/day-11-sensor_logging/calibration.h
+++ TASK 4/sketches/day-11-sensor_logging/calibration.h
@@ -1,1 +1,17 @@
+#ifndef CALIBRATION_H
+#define CALIBRATION_H
 
+// LDR Calibration
+#define LDR_DARK 95
+#define LDR_BRIGHT 910
+
+// LDR Thresholds
+#define LDR_DARK_MAX 300
+#define LDR_AMBIENT_MAX 700
+#define LDR_BRIGHT_MIN 701
+
+// Soil Moisture Calibration
+#define DRY_ADC 820
+#define WET_ADC 380
+
+#endif
