26e510f817f7a2e3c6aa254f2a6026906d699332
poojasundar860 <poojasundar860@gmail.com>
2026-06-05 13:06:15 +0530

    Update day-08-dht22.ino

--- TASK 4/sketches/day-08-dht22/day-08-dht22.ino
+++ TASK 4/sketches/day-08-dht22/day-08-dht22.ino
@@ -1,1 +1,32 @@
+#include <DHT.h>
 
+#define DHTPIN 4
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("DHT22 Sensor Test");
+  
+  dht.begin();
+}
+
+void loop() {
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("Failed to read from DHT22 sensor!");
+  } else {
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.print(" °C");
+
+    Serial.print("  |  Humidity: ");
+    Serial.print(humidity);
+    Serial.println(" %");
+  }
+
+  delay(2000);  // DHT22 requires at least 2 seconds between readings
+}
