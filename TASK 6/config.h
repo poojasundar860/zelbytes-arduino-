ca9c59b8d807082518b622f76d0055f25f2b25ec
poojasundar860 <poojasundar860@gmail.com>
2026-06-15 09:55:23 +0530

    Update config.h

--- TASK 6/config.h
+++ TASK 6/config.h
@@ -1,1 +1,17 @@
+#ifndef CONFIG_H
+#define CONFIG_H
 
+#define MOTOR_PWM_PIN  9
+#define RELAY_PIN      8
+#define ESTOP_PIN      2
+#define LED_PIN        13
+
+const int PWM_MIN = 0;
+const int PWM_MAX = 255;
+
+// ---------------- Relay Logic ----------------
+// Change if your relay is active HIGH
+#define RELAY_ON   LOW
+#define RELAY_OFF  HIGH
+
+#endif
